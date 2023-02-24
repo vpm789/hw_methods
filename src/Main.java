03218353d148f9726be1e1c4be2c1ed4bd540487
vpm789 <vpm789@gmail.com>
2023-02-24 22:32:52 +0300

    tasks

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
 public class Main {
+
+    public static void checkLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+
     public static void main(String[] args) {
         task1();
-        task2();
-        task3();
+//        task2();
+//        task3();
     }
 
     private static void task1() {
         System.out.println("");
         System.out.println("Задача №1");
+        int year = 2020;
+        checkLeapYear(year);
     }
 
     private static void task2() {
